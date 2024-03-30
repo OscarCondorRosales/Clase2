ce2f9e175edbf79a444a8d4cb8b2bd616b3c2048
CROW <CROW@SOFOS>
2024-03-29 23:45:07 -0500

    Agregar archivos de proyecto.

---
+++ Program.cs
@@ -0,0 +1,105 @@
+﻿// See https://aka.ms/new-console-template for more information
+//Console.WriteLine("Hello, World!");
+using System;
+
+//int numero1 = 0;
+//int numero2 = 0;
+//Console.WriteLine("Ingrese numero 1");
+//numero1 = Convert.ToInt32( Console.ReadLine());
+//Console.WriteLine("Ingrese numero 2");
+//numero2 = Convert.ToInt32( Console.ReadLine());
+//if (numero1 > numero2)
+//{
+//    Console.WriteLine("El Mayor es" + numero1);
+//}
+//else
+//{
+//    Console.WriteLine("El Mayor es " + numero2);
+//}
+
+//int numero = 0;
+//Console.WriteLine("Ingrese numero");
+//numero = Convert.ToInt32( Console.ReadLine());
+//if (numero % 2 == 0)
+//{
+//    Console.WriteLine("El numero" +numero+" es PAR");
+//}
+//else
+//{
+//    Console.WriteLine("El numero" +numero+" es IMPAR");
+//}
+
+//int calificacion = 0;
+//Console.WriteLine("Ingrese calificacion del 0 a 100");
+//calificacion = Convert.ToInt32(Console.ReadLine());
+//if (calificacion>=90 && calificacion<=100)
+//{
+//    Console.WriteLine(calificacion +" Excelente" );
+//}
+//else if (calificacion>=80 && calificacion<=89)
+//{
+//    Console.WriteLine(calificacion + " Muy bueno");
+//}
+//else if (calificacion >= 70 && calificacion <= 79)
+//{
+//    Console.WriteLine(calificacion + " Bueno");
+//}
+//else if (calificacion >= 60 && calificacion <= 69)
+//{
+//    Console.WriteLine(calificacion + " Suficiente");
+//}
+//else if (calificacion <= 60)
+//{
+//    Console.WriteLine(calificacion + " Insuficiente");
+//}
+
+//int numeroMes = 0;
+//string nombreMes = "";
+//Console.WriteLine("Ingrese numero mes");
+//numeroMes = Convert.ToInt32(Console.ReadLine());
+
+//switch (numeroMes)
+//{
+//    case 1:
+//        nombreMes = "Enero";
+//        break;
+//    case 2:
+//        nombreMes = "Febrero";
+//        break;
+//    case 3:
+//        nombreMes = "Marzo";
+//        break;
+//    case 4:
+//        nombreMes = "Abril";
+//        break;
+//    case 5:
+//        nombreMes = "Mayo";
+//        break;
+//    case 6:
+//        nombreMes = "Junio";
+//        break;
+//    case 7:
+//        nombreMes = "Julio";
+//        break;
+//    case 8:
+//        nombreMes = "Agosto";
+//        break;
+//    case 9:
+//        nombreMes = "Setiembre";
+//        break;
+//    case 10:
+//        nombreMes = "Octubre";
+//        break;
+//    case 11:
+//        nombreMes = "Noviembre";
+//        break;
+//    case 12:
+//        nombreMes = "Diciembre";
+//        break;
+//    default:
+//        nombreMes = "Numero Incorrecto";
+//        break;
+//}
+
+//Console.WriteLine("El nombre del mes es "+nombreMes);
+
